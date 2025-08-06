fa78980d184c27bd2158055c3d5464dc10849816
JOSE SERGIO DA SILVA <videira384318@gmail.com>
2025-08-06 22:12:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "API_KEY",
+  apiKey: "SECRET_API_KEY",
   authDomain: "fir-studiogpt-test-project.firebaseapp.com",
   projectId: "fir-studiogpt-test-project",
   storageBucket: "fir-studiogpt-test-project.appspot.com",
