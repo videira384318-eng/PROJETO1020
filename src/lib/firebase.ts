ab7759d4acf14b12d2c8de865b2b3943b226bf3b
JOSE SERGIO DA SILVA <videira384318@gmail.com>
2025-08-06 21:43:37 +0000

    Ok

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "API_KEY",
+  authDomain: "fir-studiogpt-test-project.firebaseapp.com",
+  projectId: "fir-studiogpt-test-project",
+  storageBucket: "fir-studiogpt-test-project.appspot.com",
+  messagingSenderId: "362141527833",
+  appId: "1:362141527833:web:20f8c8188151287c88b907",
+  measurementId: "G-1WJ2PZ053S"
 };
 
 // Initialize Firebase
