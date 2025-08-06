42cb95aa6ddfe7814c686882de3a090d4bc6c08e
JOSE SERGIO DA SILVA <videira384318@gmail.com>
2025-08-06 22:31:41 +0000

    E por que os registro tão sumindo quando saiu do aplicativo não é pra su

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,5 @@
+"use client";
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -17,7 +19,13 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+let app;
+if (getApps().length === 0) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
+
 const db = getFirestore(app);
 
 export { app, db };
