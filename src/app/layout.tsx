27c39cf59439affde00afc52a676764bcaabb08e
JOSE SERGIO DA SILVA <videira384318@gmail.com>
2025-08-10 00:41:06 +0000

    ME DE O PASSO A PASSO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="icon" href="/icons/icon-192x192.png" />
       </head>
       <body className="font-body antialiased">
         <AuthProvider>
