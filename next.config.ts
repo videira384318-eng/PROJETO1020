810c4efcde13f3e7df00823994f2504e30f57bd5
JOSE SERGIO DA SILVA <videira384318@gmail.com>
2025-08-18 00:21:12 +0000

    esta pedindo para verificar a chave de api

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
-
 const nextConfig = {
   /* config options here */
   typescript: {
@@ -28,4 +20,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
