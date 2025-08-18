e8af6e5f5b1b4dfc97f92133620c02e09d3251ee
JOSE SERGIO DA SILVA <videira384318@gmail.com>
2025-08-18 00:50:02 +0000

    TA A MESMA BOSTA

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,15 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { firebaseConfig } from './firebaseConfig';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Initialize Firebase
-// This check prevents re-initializing the app on every hot-reload
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+let app;
+if (getApps().length === 0) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
+
 const db = getFirestore(app);
 
 export { app, db };
